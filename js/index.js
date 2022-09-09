14c52a490e547cd3ce00ba0b1e28f67e00596d28
Alejandro Gómez <alejgolop@gmail.com>
2022-09-09 22:47:29 +0200

    s

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let items;
 async function loadItems()
 {
-    items=await fetch('data/items-real.json').then((response) => response.json());
+    items=await fetch('data/items-prod.json').then((response) => response.json());
     processItems();
 }
 
@@ -11,8 +11,8 @@ function processItems()
 $("#items").empty();
 items.forEach((item,index)=>{
     let itemTemplate=`<div class="col item-holder" onclick="window.location='detail.html?item=${index}'">
-<div class="card" title="${item.desc}">
-    <img src="${item.images[0]}" class="card-img">
+<div class="card" title="${item.name}">
+    <img src="${item.thumbnail}" class="card-img">
     <div class="card-img-overlay">
         <h5 class="card-title vip-text">${item.name}</h5>
         <div class="price">${item.price}€</div>
@@ -23,6 +23,7 @@ html+=itemTemplate;
 });
 $("#items").append(html);
 }
+
 
 $( document ).ready(function() {
     //console.log( "ready!" );
