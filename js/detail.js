c883da4b0ee7e8347842352ce8afa87ebe27ec62
Alejandro Gómez <alejgolop@gmail.com>
2022-09-09 23:11:34 +0200

    s

--- js/detail.js
+++ js/detail.js
@@ -37,7 +37,10 @@ async function loadItem()
         zoom: true,
         centeredSlides: true,
         loop: true,
-      
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
         // If we need pagination
         pagination: {
           el: '.swiper-pagination',
